22955f126af9dca0f7f643dedd82a8ce3b961f78
lls433234 <lls13627619914>
2019-09-26 13:59:57 +0800

    短信接口以及项目架构

---
+++ Invoice/src/main/java/com/jiuyao/pigx/PigxApplication.java
@@ -0,0 +1,13 @@
+package com.jiuyao.pigx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigxApplication.class, args);
+    }
+
+}
